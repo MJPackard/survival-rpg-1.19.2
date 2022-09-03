adaeee2f75e38dadb211d425830391c184cd16e6
MJPackard <maxjpackard@gmail.com>
2022-09-03 22:10:50 +0100

    Initial commit

---
+++ src/main/java/net/mjpackard/adventuresurvival/AdventureSurvival.java
@@ -0,0 +1,15 @@
+package net.mjpackard.adventuresurvival;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdventureSurvival implements ModInitializer {
+	public static final String MOD_ID = "adventuresurvival";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mjpackard/adventuresurvival/AdventureSurvivalClient.java
@@ -0,0 +1,10 @@
+package net.mjpackard.adventuresurvival;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdventureSurvivalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
